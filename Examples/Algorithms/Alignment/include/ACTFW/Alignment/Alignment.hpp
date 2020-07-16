9d8d012ca2e7ff4b9f2ea68f437f7291173e62cb
xai <xiaocong.ai@cern.ch>
2020-07-16 16:52:09 +0200

    Add print out of alignment detector geometry info

--- Examples/Algorithms/Alignment/include/ACTFW/Alignment/Alignment.hpp
+++ Examples/Algorithms/Alignment/include/ACTFW/Alignment/Alignment.hpp
@@ -427,6 +427,22 @@ struct Alignment {
       ACTS_ERROR("Alignment is not converged.");
       alignRes.result = AlignmentError::ConvergeFailure;
     }
+    // Print out the final aligned parameters
+    unsigned int iDetElement = 0;
+    for (const auto& det : alignOptions.alignedDetElements) {
+      const auto& transform =
+          det->transform(alignOptions.fitOptions.geoContext);
+      const auto& translation = transform.translation();
+      const auto& rotation = transform.rotation();
+      const Acts::Vector3D rotAngles = rotation.eulerAngles(2, 1, 0);
+      ACTS_INFO("Detector element "
+                << iDetElement
+                << " has aligned geometry position as below: \n");
+      ACTS_INFO("Center (cenX, cenY, cenZ) = " << translation.transpose());
+      ACTS_INFO("Euler angles (rotZ, rotY, rotX) = " << rotAngles.transpose());
+      ACTS_INFO("Rotation marix = \n" << rotation);
+      iDetElement++;
+    }
 
     return alignRes;
   }
